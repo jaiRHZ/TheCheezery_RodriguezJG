04e4183ac33a87b94b1f2a0f3f80d7e1fa74e0f5
jaiRHZ <jairo.rodriguez213248@potros.itson.edu.mx>
2025-09-22 01:04:34 -0700

    funcional
    Botones funcionales

---
+++ app/src/main/java/rodriguez/jairo/thecheezery_rodriguezjg/Product.kt
@@ -0,0 +1,6 @@
+package rodriguez.jairo.thecheezery_rodriguezjg
+
+data class Product (var name: String,
+                    var image: Int,
+                    var description: String,
+                    var price: Double)
